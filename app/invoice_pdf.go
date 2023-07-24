5c3cf14789e0b1420bdf24c193e55db490621aa7
Andrew Arrow <oneone@gmail.com>
2023-07-24 11:21:10 -0700

    name

--- app/invoice_pdf.go
+++ app/invoice_pdf.go
@@ -13,9 +13,17 @@ func generatePdf(c *router.Context, invoice map[string]any) {
 
 	//fontPath := "path/to/your/nice_font.ttf"
 	//pdf.AddFont("CustomFont", "", fontPath)
-	pdf.SetFont("helvetica", "", 14)
+	pdf.SetFont("helvetica", "", 10)
 
-	pdf.Text(20, 30, "Hello, this is some nice text!")
+	userId := invoice["user_id"]
+	clientId := invoice["client_id"]
+	header := c.One("template", "where flavor=$1 and user_id=$2", "header", userId)
+	headerText := header["text"].(string)
+	pdf.Text(20, 30, headerText)
+
+	client := c.One("client", "where id=$1", clientId)
+	name := client["name"].(string)
+	pdf.Text(20, 40, name)
 
 	var buffer bytes.Buffer
 	pdf.Output(&buffer)
