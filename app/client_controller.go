ba0205ee5d11546b48f62d2f1561176f80e784b6
Andrew Arrow <oneone@gmail.com>
2023-07-24 07:24:13 -0700

    new

--- app/client_controller.go
+++ app/client_controller.go
@@ -20,17 +20,19 @@ func handleClientIndex(c *router.Context) {
 	list := c.All("client", "order by created_at desc", "")
 
 	colAttributes := map[int]string{}
-	colAttributes[0] = "w-1/2"
+	//colAttributes[0] = "w-1/2"
 
 	m := map[string]any{}
-	headers := []string{"id", "name", "address", "created_at"}
+	headers := []string{"name", "created"}
 
 	params := map[string]any{}
 	m["headers"] = headers
 	m["cells"] = c.MakeCells(util.ToAnyArray(list), headers, params, "_client")
 	m["col_attributes"] = colAttributes
 
+	topVars := map[string]any{}
 	send := map[string]any{}
 	send["bottom"] = c.Template("table_show.html", m)
+	send["top"] = c.Template("clients_list_top.html", topVars)
 	c.SendContentInLayout("generic_top_bottom.html", send, 200)
 }
